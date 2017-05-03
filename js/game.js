f18b6a45cbcf1e266b2bc833607c20fd4774bd00
Roger Forner Fabre <sir.rogerforner@gmail.com>
2017-05-03 19:38:44 +0200

    tinkeringPhaser; Es veu el logo de Phaser.

--- js/game.js
+++ js/game.js
@@ -1,3 +1,22 @@
-/**
- * Created by rogerforner on 3/05/17.
- */
+//console.log('Ieic!')
+window.onload = function () {
+    // var game = new Phaser('100','100') %
+    // var game = new Phaser(800, 600, Phaser.AUTO, '#game') si es deixa buit, va al body.
+
+    var states = {
+        preload: preload,
+        create: create
+    }
+
+    var game = new Phaser.Game(800, 600, Phaser.AUTO, '', states)
+
+    function preload() {
+        //LoadManager
+        game.load.image('logo','assets/phaser.png')
+    }
+
+    function create() {
+        game.add.sprite(0,0,'logo')
+        //game.add.sprite(game.world.centerX,0, )
+    }
+}
